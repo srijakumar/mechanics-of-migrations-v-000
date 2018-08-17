b16437d3c758023cdc72d0aeb63ffe4c2fdf4d0e
Srija S <srijakumar7@gmail.com>
2018-08-17 21:10:53 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -14,15 +14,5 @@ ActiveRecord::Base.establish_connection(
 )
 # put the code to connect to the database here
 
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
 
-ActiveRecord::Base.connection.execute(sql)
 require_relative "../artist.rb"
